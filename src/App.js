88e75397227054a2c6eb493e7bca7f59cb41ebf6
alexisbedoya <39450793+alexisbedoya@users.noreply.github.com>
2021-06-16 14:40:47 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import { Header } from './components/Header.jsx';
+import {TinderCards} from './components/TinderCards.jsx';
+import {SwipeButtons} from "./components/SwipeButtons";
+function App() {
+  return (
+    //BEM class maning covention 
+
+    <div className="App">
+       
+     
+          <Header />
+          <TinderCards />
+          <SwipeButtons />
+         
+        
+    </div>
+  );
+}
+
+export default App;
